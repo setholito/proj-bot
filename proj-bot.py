8f6ea93d1ac1def83ac0aef489d8f81fd9e20b35
Seth Davis <setholito@gmail.com>
2016-01-21 13:03:54 -0600

    Add power to pull from GitHub. Add genericDownloader function. Add svnDownloader function.

--- proj-bot.py
+++ proj-bot.py
@@ -11,22 +11,110 @@ PROJ-BOT
 import shutil
 import time
 import os, sys
+import urllib
 
 # SETUP VARIABLES
 def initProj():
     projStorage =  "/Users/sdavis/Documents/_jira-tasks/"
-    #projStorage =  "/Users/sdavis/Desktop/py-playground/email-task/"
-    projFolderName = raw_input('\nEnter Name of Project: ')
+    projFolderName = raw_input('\nProject Name: ')
 
     # PATH LOCATION & FOLDER CREATION
     projPath = projStorage + (time.strftime("%Y%m%d-")) + projFolderName
-
     return projPath
 
-# FOLDER STRUCTURE CREATION
+# STRING, STRING > NULL
+def genericDownloader(urlToDownload, localLocation):
+    print ("-"*40)
+    fileName = urlToDownload.split("/")[-1]
+    print ("Downloading \"" + fileName + "\"...")
+    urllib.urlretrieve(urlToDownload, localLocation + fileName)
+    print ("Successfully Downloaded: " + fileName)
+    print ("-"*40)
+
+# STRING, STRING > NULL
+def svnDownloader(urlToDownload, localLocation):
+    fileName = urlToDownload.split("/")[-1]
+    print ("-"*40)
+    print ("SVN Downloading \"" + fileName + "\"...")
+    os.system("svn export " + urlToDownload + " " + localLocation)
+    print ("SVN Successfully Downloaded: " + fileName)
+    print ("-"*40)
+
+# STRING > STRING
+def renameFile(oldName, newName):
+    os.rename(oldName, newName)
+
+# STRING > RETURN NULL
+def addEmailAssets(emailDestination):
+
+    siteAbbrev = raw_input('Site Abbreviation? ').lower()   
+    emailDestinationString = str(emailDestination)
+
+    # PROJECT
+    projWorkingFolderPath = emailDestinationString + "/_working/"
+    projResourcesFolderPath = emailDestinationString + "/_resources/"
+
+    # ACCESS URLLIB OPENER
+    theOpener = urllib.URLopener()
+
+    # VARIABLES FOR GITHUB
+    gitEmailParentDirectory = "https://github.com/livingdirectcreative/2015-responsive-email-templates/trunk/_working/" + siteAbbrev + "-email-template"
+    gitEmailImagesPath = gitEmailParentDirectory + "/images"
+    gitEmailShellPath = "https://raw.githubusercontent.com/livingdirectcreative/2015-responsive-email-templates/master/_working/" + siteAbbrev + "-email-template/" + siteAbbrev + "-email-template-shell.html"
+    
+    # PROJECT VARIABLES
+    projDirName = gitEmailParentDirectory.split("/")[-1]
+    projImageDirName = gitEmailImagesPath.split("/")[-1]
+
+    # DESTINATION VARIABLES 
+    projTemplateDestination = projWorkingFolderPath + "/" + projDirName
+    projTemplateImageDestination = projWorkingFolderPath + "/" + projImageDirName
+    projEmailShellDestination = projWorkingFolderPath
+
+    # DOWNLOAD SHELL
+    genericDownloader(gitEmailShellPath, projEmailShellDestination)
+    # DOWNLOAD IMAGES
+    svnDownloader(gitEmailImagesPath, projTemplateImageDestination)
+    
+    # RENAME FILE VARIABLES
+    intermediateProjFolderName = os.path.dirname(projWorkingFolderPath)
+    targetFileName = projWorkingFolderPath + siteAbbrev + "-email-template-shell.html"
+    renameEmailToProjName = intermediateProjFolderName.split("/")[-2] + ".html"
+
+    renameFile(targetFileName, projWorkingFolderPath + renameEmailToProjName)
+
+    print ("Renamed Email:"), renameEmailToProjName
+    
+    # COPY SNIPPETS TO RESOURCES?
+    addSnippets = raw_input('Snippets? y/n: ').lower()
+
+    if addSnippets == "y":
+        fileToDownload = "https://github.com/livingdirectcreative/2015-responsive-email-templates/trunk/_working/_snippets"
+        parsedFileName = fileToDownload.split("/")[-1]
+        fileDestination = projResourcesFolderPath + parsedFileName
+        svnDownloader(fileToDownload, fileDestination)
+    elif addSnippets != "n":
+        "'y' or 'n'. Try again..."
+
+    # COPY PSDS TO RESOURCES?
+    addEmailPsdTemplate = raw_input('Banner PSD? y/n: ').lower()
+
+    emailBannerCollection = [
+                             "https://github.com/livingdirectcreative/psd-templates/raw/master/email/featured-banner-template.psd",
+                             "https://github.com/livingdirectcreative/psd-templates/raw/master/email/skinny-banner-template.psd"
+                            ]
+
+    if addEmailPsdTemplate == "y":
+        for emailPsdUrl in emailBannerCollection:
+            genericDownloader(emailPsdUrl, projResourcesFolderPath)
+    elif addEmailPsdTemplate != "n":
+        "Hey wise guy. Try again..."
+
+# NULL
 def generateProj():
 
     projPath = initProj()
+    print "PROJECT PATH: ", projPath
 
     # FOLDER LIST
     projFolderList = ['_resources','_working','deliverables']
@@ -39,59 +127,11 @@ def generateProj():
        print 'Folder Added: ' + projFolder
 
     print "------"
-
-    # EMAIL PROJ CREATION FUNCTION
-    def makeEmailProj():
-
-        siteAbbrev = raw_input('Site Abbreviation: ').lower()
-        
-        projWorkingFolderPath = projPath + "/_working/"
-        projResourcesFolderPath = projPath + "/_resources/"
-
-        templateWorkingFolderPath = "/Users/sdavis/Documents/repositories/2015-responsive-email-templates/_working/"
-        templateEmailPath = "/Users/sdavis/Documents/repositories/2015-responsive-email-templates/_working/" + siteAbbrev + "-email-template/"
-        templateResourcePath = "/Users/sdavis/Documents/repositories/2015-responsive-email-templates/_working/_featured-banners"
-
-        # COPY FILES & FOLDERS FROM EMAIL TEMPLATE DIRECTORY
-        for root, dirs, files in os.walk(templateEmailPath, topdown=False):
-            # COPY EMAIL SHELL FILE
-            for name in files:
-                if name == siteAbbrev + "-email-template-shell.html":
-                    templateShellFilePath = os.path.join(root, name)
-                    shutil.copy (templateShellFilePath, projWorkingFolderPath + name)
-            # COPY IMAGES FOLDER
-            for name in dirs:
-                if name == "images":
-                    templateImagesFilePath = os.path.join(root, name)
-                    shutil.copytree (templateImagesFilePath, projWorkingFolderPath + name)
-        
-        # COPY SNIPPETS TO RESOURCES?
-        addSnippets = raw_input('Add _snippets folder to _resources? y/n: ').lower()
-
-        if addSnippets == "y":
-            # COPY SNIPPETS FROM EMAIL TEMPLATE DIRECTORY
-            for root, dirs, files in os.walk(templateWorkingFolderPath, topdown=False):
-                # COPY SNIPPETS FOLDER
-                for name in dirs:
-                    if name == "_snippets":
-                        templateSnippetsPath = os.path.join(root, name)
-                        shutil.copytree (templateSnippetsPath, projResourcesFolderPath + name)
-
-        # COPY PSDS TO RESOURCES?
-        addPSDTemplate = raw_input('Add PSD templates to _resources? y/n: ').lower()
-
-        if addPSDTemplate == "y":
-            # COPY PSD FROM BANNERS DIRECTORY
-            for root, dirs, files in os.walk(templateResourcePath, topdown=False):
-                for name in files:
-                    if name == "featured-banner-template.psd" or name == "skinny-banner-template.psd":
-                        templateBannerPath = os.path.join(root, name)
-                        shutil.copy (templateBannerPath, projPath + "/_resources/")
-
+    
     # ADD EMAIL TEMPLATES
     addEmailTemplate = raw_input('Add email templates? y/n: ').lower()
     if addEmailTemplate == "y":
-        makeEmailProj()
+        addEmailAssets(projPath)
 
     # ADD ANOTHER PROJECT?
     addAnotherProj = raw_input('Add another project? y/n: ').lower()
